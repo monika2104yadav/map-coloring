5d984dd1c3f49310e64bb73a4997d270d21836d1
monika2104yadav <45217870+monika2104yadav@users.noreply.github.com>
2020-06-19 00:56:41 +0530

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,123 @@
+
+#include <cstdint>
+#include <iostream>
+#include <cstdio>
+#include <map>
+
+#include <vector>
+#include <string>
+#include <list>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct node {
+	int value;
+	list<node*> neighbors;
+	bool visited;
+	int assigned_color;
+	node(int val) {
+		value = val;
+		visited = false;
+	}
+};
+
+class graph {
+public:
+	list<node*> all_nodes;
+};
+
+void graph_coloring(graph input_graph, vector<int> color) {
+	//vector<int> ret;
+
+	list<node*> queue;
+	queue.push_back(input_graph.all_nodes.front());
+
+	while (!(queue.empty())) {
+		node* current = queue.front();
+		queue.pop_front();
+		current->visited = true;
+
+		map<int, int> neighbor_color_map;
+		list<node*>::iterator neigh_it = current->neighbors.begin();
+		for (neigh_it; neigh_it != current->neighbors.end(); ++neigh_it) {
+			if ((*neigh_it)->visited == false) {
+				queue.push_back(*neigh_it);
+			}
+			if ((*neigh_it)->visited == true) {
+				neighbor_color_map.insert({ (*neigh_it)->assigned_color, (*neigh_it)->assigned_color });
+			}
+		}
+		if (neighbor_color_map.size() != 0) {
+			for (int i = 0; i < color.size(); i++) {
+				if (neighbor_color_map.find(color[i]) != neighbor_color_map.end()) {
+					continue;
+				} else {
+					current->assigned_color = color[i];
+					break;
+				}
+			}
+		}
+		else current->assigned_color = color[0];
+	}
+}
+
+
+int main() {
+	node* n1 = new node(1);
+	node* n2 = new node(2);
+	node* n3 = new node(3);
+	node* n4 = new node(4);
+	node* n5 = new node(5);
+
+	list<node*> l1;
+	l1.push_back(n2);
+	l1.push_back(n3);
+	l1.push_back(n4);
+
+	n1->neighbors = l1;
+
+	list<node*> l2;
+	l2.push_back(n4);
+	l2.push_back(n5);
+	l2.push_back(n1);
+	n2->neighbors = l2;
+
+	list<node*> l3;
+	l3.push_back(n1);
+	l3.push_back(n4);
+	n3->neighbors = l3;
+
+	list<node*> l4;
+	l4.push_back(n1);
+	l4.push_back(n2);
+	l4.push_back(n3);
+	l4.push_back(n5);
+	n4->neighbors = l4;
+
+	list<node*> l5;
+	l5.push_back(n2);
+	l5.push_back(n4);
+	n5->neighbors = l5;
+
+	list<node*> all_nodes;
+	all_nodes.push_back(n1);
+	all_nodes.push_back(n2);
+	all_nodes.push_back(n3);
+	all_nodes.push_back(n4);
+	all_nodes.push_back(n5);
+
+	graph g; g.all_nodes = all_nodes;
+
+	vector<int> color = { 1, 2, 3, 4 };
+	graph_coloring(g, color);
+
+	list<node*>::iterator node_it = all_nodes.begin();
+	for (node_it; node_it != all_nodes.end(); node_it++) {
+		cout << (*node_it)->assigned_color << endl;
+	}
+	cin.get();
+}
